f797fa18abce0dcf6e556827403a42f31d623d5f
sanjairocky <talk2sanjai@gmail.com>
2021-05-02 04:20:56 +0530

    Update worker.js

--- chat/worker.js
+++ chat/worker.js
@@ -25,7 +25,7 @@ self.addEventListener('install', function(event) {
       caches.open(CACHE_NAME)
         .then(function(cache) {
           const urlsToCache = [
-                'index.html', '/', 'static/js/2.580e3f16.chunk.js.LICENSE.txt','static/js/runtime-main.7a6efe9b.js',
+                'index.html', '/', '', 'static/js/2.580e3f16.chunk.js.LICENSE.txt','static/js/runtime-main.7a6efe9b.js',
                 'logo.png','static/css/main.162f152b.chunk.css','static/js/main.542e1f63.chunk.js','static/js/2.580e3f16.chunk.js','manifest.webmanifest'
               ];
           cache.addAll(urlsToCache);
@@ -37,6 +37,7 @@ self.addEventListener('install', function(event) {
 // Here we intercept request and serve up the matching files
 self.addEventListener('fetch', function(event) {
   if (doCache) {
+    console.log(event.request);
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
