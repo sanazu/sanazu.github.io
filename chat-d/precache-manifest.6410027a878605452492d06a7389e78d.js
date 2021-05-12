437fc7c8a79d187e352c7c0209ff89675cbb9d4d
sanjairocky <talk2sanjai@gmail.com>
2021-05-12 17:29:53 +0530

    Add files via upload

---
+++ chat-d/precache-manifest.6410027a878605452492d06a7389e78d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660b39f53a8ad8cbbdd9929e8b6256d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdacbb0151258a6d8a43",
+    "url": "/static/css/main.e69844f1.chunk.css"
+  },
+  {
+    "revision": "5d7c48ee35f2a04527de",
+    "url": "/static/js/2.ac80d819.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.ac80d819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdacbb0151258a6d8a43",
+    "url": "/static/js/main.ae368056.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/default.adbccee0.png"
+  },
+  {
+    "revision": "430bcc9ad54eaf7cdff8b230b6cbe543",
+    "url": "/static/media/dp.430bcc9a.jpg"
+  }
+]);
