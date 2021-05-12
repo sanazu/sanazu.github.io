a0415bd7d5e09d8e599338a7b67692e3bf7dbfe2
sanjairocky <talk2sanjai@gmail.com>
2021-05-12 14:53:28 +0530

    project init

---
+++ chat-d/sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "version-1";
+
+const self = this;
+
+(async () => {
+  const { files, offlineMode } = await fetch("asset-manifest.json").then(
+    (data) => data.json()
+  );
+  let urlsToCache = Object.values(files).map((a) => a.substr(1));
+  // load SW
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("sw.js")
+        .then((reg) => console.log("Success: ", reg.scope))
+        .catch((err) => console.log("Failure: ", err));
+    });
+  }
+
+  // Install SW
+  self.addEventListener("install", async (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Opened cache");
+
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  // Listen for requests
+  self.addEventListener("fetch", (event) => {
+    let local = caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    });
+    let offline = caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match(event.request));
+    });
+    event.respondWith(offlineMode ? offline : local);
+  });
+
+  // Activate the SW
+  self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        )
+      )
+    );
+  });
+})();
